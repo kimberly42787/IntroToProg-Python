578658226fec02644304289761267bfdb8488b2d
Kim Domingo <kim@Kims-MacBook-Pro-2.local>
2023-08-16 22:00:51 -0700

    Committed assignment01

---
+++ Assignment01/Assignment01.py
@@ -0,0 +1,12 @@
+
+
+
+def enter_name():
+    first_name = input("Enter first name: ")
+    last_name = input("Enter last name: ")
+    print("My name is ",first_name, last_name)
+
+print("Please introduce yourself")
+enter_name()
+
+
